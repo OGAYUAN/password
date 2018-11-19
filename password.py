02f84dd4887a580a37c945ae4987e4c098164f98
unknown <scyuan4@gmail.com>
2018-11-19 22:09:58 +0800

    pswnew2

--- password.py
+++ password.py
@@ -2,15 +2,18 @@
 #password = a123456
 password = 'a123456'
 y = 3 #剩餘次數
-while True:#當
+while y > 0:#當剩餘次數大於0次執行迴圈
+	y = y - 1#當失敗的時候剩餘次數遞減
 	key = input('請輸入密碼: ')
 	if key == password:
 		print('登入成功')
 		break #離開迴圈
 	else:
-		y = y - 1
-		print('密碼錯誤! 還有',  y, '次機會')
-		if y == 0:
-			break
-			
+		print('登入失敗')#當剩餘次數=0時值行
+		print('帳號已鎖定')
+		if y > 0:#當剩餘次數>0時值行
+			print('密碼錯誤! 還有',  y, '次機會')
+		else:
+			print('請重新驗證')
+
 
